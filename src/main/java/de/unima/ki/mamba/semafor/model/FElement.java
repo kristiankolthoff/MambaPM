6c64a157c62996a2018b033c37ec53d7b4a8dc1d
kristiankolthoff <k_kolthoff@web.de>
2015-12-19 17:11:19 +0100

    POJOs for Frame and FElements

--- src/main/java/de/unima/ki/mamba/semafor/model/FElement.java
+++ src/main/java/de/unima/ki/mamba/semafor/model/FElement.java
@@ -2,4 +2,25 @@ package de.unima.ki.mamba.semafor.model;
 
 public class FElement {
 
+	private String name;
+	private String content;
+	
+	public FElement(String name, String content) {
+		this.name = name;
+		this.content = content;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getContent() {
+		return content;
+	}
+	public void setContent(String content) {
+		this.content = content;
+	}
+	
+	
 }
--- src/main/java/de/unima/ki/mamba/semafor/model/Frame.java
+++ src/main/java/de/unima/ki/mamba/semafor/model/Frame.java
@@ -1,5 +1,51 @@
 package de.unima.ki.mamba.semafor.model;
 
+import java.util.List;
+
 public class Frame {
 
+	private String sentence;
+	private String name;
+	private String target;
+	private List<FElement> fElements;
+	
+	public Frame(String sentence, String name, String target, List<FElement> fElements) {
+		this.sentence = sentence;
+		this.target = target;
+		this.name = name;
+		this.fElements = fElements;
+	}
+
+	public String getSentence() {
+		return sentence;
+	}
+
+	public void setSentence(String sentence) {
+		this.sentence = sentence;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public List<FElement> getfElements() {
+		return fElements;
+	}
+
+	public void setfElements(List<FElement> fElements) {
+		this.fElements = fElements;
+	}
+
+	public String getTarget() {
+		return target;
+	}
+
+	public void setTarget(String target) {
+		this.target = target;
+	}
+	
 }
